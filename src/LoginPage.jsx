a812d22153035a912e2faf3be2d078304df52be4
aqeeebb <aqeeebb@gmail.com>
2025-08-12 16:46:29 +0500

    Update LoginPage.jsx

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -1,32 +1,48 @@
 import React from 'react';
 import './Lstyles.css'; 
-import bg1 from './assets/bg1.jpg'
+import bg1 from './assets/bg1.jpg';
+
 const LoginPage = () => {
   return (
     <div className="container">
-       
-        <img
-        src={bg1} alt="Graphics Image" />
-      
+      {/* Fixed alt text: Removed the word "Image" */}
+      <img
+        src={bg1}
+        alt="Login page background"
+      />
+
       <div className="login">
         <h3 className="title">User Login</h3>
+        
         <div className="text-input">
           <i className="ri-user-fill"></i>
           <input type="text" placeholder="Username" />
         </div>
+        
         <div className="text-input">
           <i className="ri-lock-fill"></i>
           <input type="password" placeholder="Password" />
         </div>
+        
         <button className="login-btn">LOGIN</button>
-        <a href="#" className="forgot">Forgot Username/Password?</a>
+        
+        {/* Fixed invalid href: changed to a button for non-navigation */}
+        <button
+          className="forgot"
+          onClick={() => alert("Forgot password flow not implemented yet")}
+          style={{ background: "none", border: "none", color: "blue", cursor: "pointer", padding: 0 }}
+        >
+          Forgot Username/Password?
+        </button>
+        
         <div className="create">
-          <a href="#">Create Your Account</a>
+          {/* Changed to a real route link (can adjust as per your app) */}
+          <a href="/signup">Create Your Account</a>
           <i className="ri-arrow-right-fill"></i>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
